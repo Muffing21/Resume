35d884c56e63a031c6a22f2158c8fc6975c0af1a
wichapas pichetpongsa <wpichetp@ucsc.edu>
2021-10-16 00:17:31 +0700

    partial adding

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -1,14 +1,17 @@
 #include "insert.h"
+#include "stats.h"
 
-void insertion_sorts(Stats *stats, uint32_t *A, uint32_t n){
-	for(i = 1; i < A; i++){
+void insertion_sort(Stats *stats, uint32_t *A, uint32_t n){
+	for(uint32_t i = 1; i < n; i++){
 		int element_index = i;
-		uint32_t element_num = A[i];
-		while(element_index > 0 && element_num < A[element_index-1]){
-			A[element_index] = A[element_index-1]; //if current element is lower than the previous
+		uint32_t element_num = move(stats, A[i]);
+		while((element_index > 0) && (cmp(stats, element_num, A[element_index-1]) == -1)){
+			A[element_index] = move(stats, A[element_index-1]); //if current element is lower than the previous
 			element_index -= 1;
-		}
-		A[element_index] = element_num
-	}
 
+			
+		}
+		A[element_index] = move(stats, element_num);
+	}
+	
 }
