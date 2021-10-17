40a6b56e5abf64791a68081bbe9b82aa8df92f43
wichapas pichetpongsa <wpichetp@ucsc.edu>
2021-10-17 17:27:42 +0700

    pipe linecheck

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -1,29 +1,27 @@
 #include "quick.h"
+
 #include "stats.h"
 
-
-uint32_t partition(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi){
-	uint32_t i = lo - 1;
-	for (uint32_t j = lo; j < hi; j++){
-		if (cmp(stats, A[j-1], A[hi-1]) == -1){
-			i += 1;
-			swap(stats, &A[j-1], &A[i-1]);
-		}
-	
-	}
-	swap(stats, &A[hi-1], &A[i]);
-	return i+1;	
+uint32_t partition(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
+    uint32_t i = lo - 1;
+    for (uint32_t j = lo; j < hi; j++) {
+        if (cmp(stats, A[j - 1], A[hi - 1]) == -1) {
+            i += 1;
+            swap(stats, &A[j - 1], &A[i - 1]);
+        }
+    }
+    swap(stats, &A[hi - 1], &A[i]);
+    return i + 1;
 }
 
-void quick_sorter(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi){
-	if(lo < hi){
-		uint32_t p = partition(stats, A, lo, hi);
-		quick_sorter(stats, A, lo, p-1);
-		quick_sorter(stats, A, p + 1, hi);
-	}
+void quick_sorter(Stats *stats, uint32_t *A, uint32_t lo, uint32_t hi) {
+    if (lo < hi) {
+        uint32_t p = partition(stats, A, lo, hi);
+        quick_sorter(stats, A, lo, p - 1);
+        quick_sorter(stats, A, p + 1, hi);
+    }
 }
 
-void quick_sort(Stats *stats, uint32_t *A, uint32_t n){
-	quick_sorter(stats, A , 1, n);
-
+void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
+    quick_sorter(stats, A, 1, n);
 }
