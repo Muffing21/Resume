a15c5ac864db62cf4c89dbd288d1e1bda6911b94
wichapas pichetpongsa <wpichetp@ucsc.edu>
2021-10-18 09:41:52 +0700

    done with asgn3

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -2,6 +2,7 @@
 #include "heap.h"
 #include "insert.h"
 #include "quick.h"
+#include "set.h"
 #include "shell.h"
 #include "stats.h"
 
@@ -12,7 +13,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define OPTIONS "aeisqrnp"
+#define OPTIONS "aeisqhr:n:p:"
 //style and format came from TA eugene
 //-a enables all sorting algorithms
 //-e enables heap sort
@@ -32,25 +33,24 @@ int insert_counter = 0;
 
 uint32_t element_size = default_size;
 uint32_t the_seed = DEFAULT_SEED;
+uint32_t limit_print = default_size;
 
 int main(int argc, char **argv) {
+    Set s = 0;
     int opt = 0;
-    //	bool get_a = false;
-    bool get_e = false;
-    bool get_i = false;
-    bool get_s = false;
-    bool get_q = false;
+    enum sorts { get_a, get_e, get_i, get_s, get_q };
     bool get_r = false;
     bool get_n = false;
-    //	bool get_p = false;
+    bool get_p = false;
+    bool get_h = false;
 
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
-            //		case 'a': get_a = true; break;
-        case 'e': get_e = true; break;
-        case 'i': get_i = true; break;
-        case 's': get_s = true; break;
-        case 'q': get_q = true; break;
+        case 'a': s = insert_set(get_a, s); break;
+        case 'e': s = insert_set(get_e, s); break;
+        case 'i': s = insert_set(get_i, s); break;
+        case 's': s = insert_set(get_s, s); break;
+        case 'q': s = insert_set(get_q, s); break;
         case 'r':
             get_r = true;
             the_seed = atoi(optarg);
@@ -59,12 +59,20 @@ int main(int argc, char **argv) {
             get_n = true;
             element_size = atoi(optarg);
             break;
-            //		case 'p': get_p = true; uint32_t limit_element = atoi(optarg); break;
+        case 'p':
+            get_p = true;
+            limit_print = atoi(optarg);
+            if (element_size < limit_print) {
+                limit_print = element_size;
+            }
+            break;
+        case 'h': get_h = true; break;
         }
     }
-    if (get_i) {
 
-        uint32_t A[element_size];
+    uint32_t *A = (uint32_t *) calloc(element_size, sizeof(uint32_t));
+    if (member_set(get_i, s)) {
+
         srandom(the_seed);
         for (uint32_t i = 0; i < element_size; i++) {
             A[i] = random() & 0x3FFFFFFF;
@@ -72,9 +80,10 @@ int main(int argc, char **argv) {
         Stats x;
         reset(&x);
         insertion_sort(&x, A, element_size);
-        printf("Insertion Sort, %d elements, %13" PRIu64 " moves, %13" PRIu64 " compares\n",
+
+        printf("Insertion Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n",
             element_size, x.moves, x.compares);
-        for (uint32_t i = 0; i < element_size; i++) {
+        for (uint32_t i = 0; i < limit_print; i++) {
             printf("%13" PRIu32, A[i]);
             insert_counter += 1;
             if (insert_counter == 5) {
@@ -83,8 +92,8 @@ int main(int argc, char **argv) {
             }
         }
     }
-    if (get_s) {
-        uint32_t A[element_size];
+    if (member_set(get_s, s)) {
+
         srandom(the_seed);
         for (uint32_t i = 0; i < default_size; i++) {
             A[i] = random() & 0x3FFFFFFF;
@@ -92,9 +101,10 @@ int main(int argc, char **argv) {
         Stats x;
         reset(&x);
         shell_sort(&x, A, element_size);
-        printf("Shell Sort, %d elements, %13" PRIu64 " moves, %13" PRIu64 " compares\n",
-            element_size, x.moves, x.compares);
-        for (uint32_t i = 0; i < element_size; i++) {
+
+        printf("Shell Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n", element_size,
+            x.moves, x.compares);
+        for (uint32_t i = 0; i < limit_print; i++) {
             printf("%13" PRIu32, A[i]);
             insert_counter += 1;
             if (insert_counter == 5) {
@@ -103,8 +113,8 @@ int main(int argc, char **argv) {
             }
         }
     }
-    if (get_e) {
-        uint32_t A[element_size];
+    if (member_set(get_e, s)) {
+
         srandom(the_seed);
         for (uint32_t i = 0; i < element_size; i++) {
             A[i] = random() & 0x3FFFFFFF;
@@ -112,9 +122,9 @@ int main(int argc, char **argv) {
         Stats x;
         reset(&x);
         heap_sort(&x, A, element_size);
-        printf("Heap Sort, %d elements, %13" PRIu64 " moves, %13" PRIu64 " compares\n",
-            element_size, x.moves, x.compares);
-        for (uint32_t i = 0; i < element_size; i++) {
+        printf("Heap Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n", element_size,
+            x.moves, x.compares);
+        for (uint32_t i = 0; i < limit_print; i++) {
             printf("%13" PRIu32, A[i]);
             insert_counter += 1;
             if (insert_counter == 5) {
@@ -123,8 +133,8 @@ int main(int argc, char **argv) {
             }
         }
     }
-    if (get_q) {
-        uint32_t A[element_size];
+    if (member_set(get_q, s)) {
+
         srandom(the_seed);
         for (uint32_t i = 0; i < element_size; i++) {
             A[i] = random() & 0x3FFFFFFF;
@@ -132,9 +142,9 @@ int main(int argc, char **argv) {
         Stats x;
         reset(&x);
         quick_sort(&x, A, element_size);
-        printf("quick Sort, %d elements, %13" PRIu64 " moves, %13" PRIu64 " compares\n",
-            element_size, x.moves, x.compares);
-        for (uint32_t i = 0; i < element_size; i++) {
+        printf("quick Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n", element_size,
+            x.moves, x.compares);
+        for (uint32_t i = 0; i < limit_print; i++) {
             printf("%13" PRIu32, A[i]);
             insert_counter += 1;
             if (insert_counter == 5) {
@@ -143,4 +153,58 @@ int main(int argc, char **argv) {
             }
         }
     }
+    if (member_set(get_a, s)) {
+
+        for (uint32_t r = 0; r < 4; r++) {
+            srandom(the_seed);
+            for (uint32_t i = 0; i < element_size; i++) {
+                A[i] = random() & 0x3FFFFFFF;
+            }
+            Stats x;
+            reset(&x);
+            if (r == 0) {
+                heap_sort(&x, A, element_size);
+                printf("Heap Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n",
+                    element_size, x.moves, x.compares);
+            }
+            if (r == 1) {
+                shell_sort(&x, A, element_size);
+                printf("Shell Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n",
+                    element_size, x.moves, x.compares);
+            }
+            if (r == 2) {
+                insertion_sort(&x, A, element_size);
+                printf("Insertion Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n",
+                    element_size, x.moves, x.compares);
+            }
+            if (r == 3) {
+                quick_sort(&x, A, element_size);
+                printf("quick Sort, %d elements, %1" PRIu64 " moves, %1" PRIu64 " compares\n",
+                    element_size, x.moves, x.compares);
+            }
+            for (uint32_t i = 0; i < limit_print; i++) {
+                printf("%13" PRIu32, A[i]);
+                insert_counter += 1;
+                if (insert_counter == 5) {
+                    printf("\n");
+                    insert_counter = 0;
+                }
+            }
+        }
+    }
+    if (get_h) {
+        printf("SYNOPSIS\n   A collection of comparison-based sorting algorithms.\n\nUSAGE\n   "
+               "./sorting [-haeisqn:p:r:] [-n length] [-p elements] [-r seed]\n\nOPTIONS\n");
+        printf("   -h             display program help and usage.\n");
+        printf("   -a             enable all sorts.\n");
+        printf("   -e             enable Heap Sort.\n");
+        printf("   -i             enable insertion Sort.\n");
+        printf("   -s             enable Shell sort.\n");
+        printf("   -q             enable Quick Sorts.\n");
+        printf("   -n length      specify number of array elements (default: 100).\n");
+        printf("   -p elements    specify number of elements to print (default: 100).\n");
+        printf("   -r seed        specify random seed (default: 13371453).\n");
+    }
+    free(A);
+    return 0;
 }
