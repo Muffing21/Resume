0bb5f178a159c99c0b0217dbd9df99fc57a0ce3a
wichapas pichetpongsa <wpichetp@ucsc.edu>
2021-10-24 09:18:07 +0700

    check

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -2,12 +2,13 @@
 #include "graph.h"
 #include "vertices.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 struct Graph {
-   uint32_t vertices;
-   bool undirected;
-   bool visited[VERTICES];
-   uint32_t matrix[VERTICES][VERTICES];
+   uint32_t vertices;                     //number of verticesd
+   bool undirected;                       // undirected graph
+   bool visited[VERTICES];                //where have we gone?
+   uint32_t matrix[VERTICES][VERTICES];   //adjency matrixd
 };
 
 
@@ -29,8 +30,8 @@ uint32_t graph_vertices(Graph *G){
 }
 
 bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k){
-   if (G->undirected = true){
-      matrix[j][i] = k
+   if (G->undirected == true){
+      G->matrix[j][i] = k;
    }
    if (i >= 1 && j >= i && j <= G->vertices){ 
       return true;
@@ -39,38 +40,38 @@ bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k){
 }
 
 bool graph_has_edge(Graph *G, uint32_t i, uint32_t j){
-   if (i >= 1 && j >= i && j <= G->vertices && matrix[i][j] > 0){
-      return true
+   if (i >= 1 && j >= i && j <= G->vertices && G->matrix[i][j] > 0){
+      return true;
    }
-   return false
+   return false;
 }
 
 uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j){
-   if (i >= 1 && j >= i && j <= G->vertices && matrix[i][j] > 0){
-      return matrix[i][j];
+   if (i >= 1 && j >= i && j <= G->vertices && G->matrix[i][j] > 0){
+      return G->matrix[i][j];
    }
    return 0;
 }
 
 bool graph_visited(Graph *G, uint32_t v){
-   if(visited[v] = true;){
+   if(G->visited[v] == true){
       return true;
    }
    return false;
 }
 
 void graph_mark_visited(Graph *G, uint32_t v){
-   if (i >= 1 && j >= i && j <= G->vertices){
-      visited[v] = true;
-      
-}
-
-void graph_mark_unvisited(Graph *G, uint32_t v){
-   if (i >= 1 && j >= i && j <= G->vertices){
-      visited[v] = false;
+   if (v > 0){
+      G->visited[v] = true;
    }
 }
 
-void graph_print(Graph *G){
-printf("%d %d", graph_edge_weight, graph_edge_weight)
+void graph_mark_unvisited(Graph *G, uint32_t v){
+   if (v > 0){
+      G->visited[v] = false;
+   }
 }
+
+//void graph_print(Graph *G){
+//printf("%d %d", graph_edge_weight, graph_edge_weight);
+//}
---
+++ asgn4/stack.c
@@ -0,0 +1,93 @@
+#include "stack.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Stack {
+   uint32_t top;      //index of the next empty slot
+   uint32_t capacity; //number of items that can be pushed
+   uint32_t *items;   // Array of items, each with type uint32_t
+};
+
+Stack *stack_create(uint32_t capacity){
+   Stack *s = (Stack *) malloc(sizeof(Stack));
+   if (s) {
+      s->top = 0;
+      s->capacity = capacity;
+      s->items = (uint32_t *) calloc(capacity, sizeof(uint32_t));
+      if(!s->items){
+         free(s);
+         s = NULL;
+      }
+   }
+   return s;
+}
+
+void stack_delete(Stack **s){
+   if(*s && (*s)->items){
+      free((*s)->items);
+      free(*s);
+      *s = NULL;
+   }
+   return;
+}
+
+uint32_t stack_size(Stack *s){
+   return s->top;
+}
+bool stack_empty(Stack *s){
+   if(s->top == 0){
+      return true;
+   }
+   return false;
+}
+bool stack_full(Stack *s){
+   if (s->capacity == s->top){
+      return true;
+   }
+   return false;
+}
+
+bool stack_push(Stack *s, uint32_t x){
+   if(s->capacity == s->top){
+      return false;
+   }
+   s->items[s->top] = x;
+   s->top += 1;
+   return true;
+}
+bool stack_pop(Stack *s, uint32_t *x){
+   if (s->top == 0){
+      return false;
+   }
+   s->top -= 1;
+   *x = s->items[s->top];
+   return true;
+}
+bool stack_peek(Stack *s, uint32_t *x){
+   if (s->top == 0){
+      return false;
+   }
+   *x = s->items[s->top];
+   return true;
+}
+void stack_copy(Stack *dst, Stack *src){
+   dst->capacity = src->capacity;
+   dst->top = src->top;
+   for (uint32_t z = 0; z < dst->top; z++) {
+        dst->items[z] = src->items[z];
+    }
+
+}
+
+void stack_print(Stack *s, FILE *outfile, char *cities[]){
+   for (uint32_t i = 0; i < s->top; i += 1){
+      fprintf(outfile, "%s", cities[s->items[i]]);
+      if (i + 1 != s->top){
+         fprintf(outfile, " -> ");
+      }
+   }
+   fprintf(outfile, "\n");
+}   
+
